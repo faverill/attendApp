58956d4c852563b4afd7a813d78b211e3072d0ef
Frank W. Averill <faverill45@att.net>
2017-07-03 16:29:42 -0400

    Courses with only impermanent data.

--- ClientApp/app/app.module.shared.ts
+++ ClientApp/app/app.module.shared.ts
@@ -1,28 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './components/app/app.component'
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
+
+import { CoursesComponent } from './components/courses/courses.component';
+import { CourseListComponent } from './components/courses/course-list/course-list.component';
+import { CourseDetailComponent } from './components/courses/course-detail/course-detail.component';
+import { CourseItemComponent } from './components/courses/course-list/course-item/course-item.component';
+import { CourseEditComponent } from './components/courses/course-edit/course-edit.component';
+import { DropdownDirective } from './components/shared/dropdown.directive';
+import { CoursesService } from './components/courses/courses.service';
+
+//Questionable imports
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+
 
 export const sharedConfig: NgModule = {
     bootstrap: [ AppComponent ],
     declarations: [
         AppComponent,
         NavMenuComponent,
-        HomeComponent
+		CoursesComponent,
+		CourseListComponent,
+		CourseDetailComponent,
+		CourseItemComponent,
+		CourseEditComponent,
+		DropdownDirective,
+
     ],
 	imports: [
 		BrowserModule,
+		ReactiveFormsModule,
 		FormsModule,
 		HttpModule,
         RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: '**', redirectTo: 'home' }
+            { path: '', redirectTo: 'courses', pathMatch: 'full' },
+			{ path: 'courses', component: CoursesComponent },
+			{ path: 'courses/:id', component: CourseDetailComponent },
+			{ path: 'courses/:id/:mode', component: CourseEditComponent },
+            { path: '**', redirectTo: 'courses' }
         ])
-    ]
+	],
+	providers: [CoursesService]
 };
